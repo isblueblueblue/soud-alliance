50455cb2494a9aed949236b004f59dbd65855dd6
isblueblueblue <isblueblueblue@gmail.com>
2022-05-04 10:16:56 +0900

    Update index.html

--- js/action.js
+++ js/action.js
@@ -6,6 +6,15 @@ $(function () {
 
 function common() {}
 
-function main() {}
+function main() {
+  $(".event__items").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev'>Previous</button>",
+    nextArrow: "<button type='button' class='slick-next'>Next</button>",
+  });
+}
 
 function sub() {}
