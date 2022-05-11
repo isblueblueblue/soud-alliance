e527e21baffff325eed4aefb33ac5ea96b210511
isblueblueblue <isblueblueblue@gmail.com>
2022-05-11 14:05:31 +0900

    Edit navbar.css

--- js/action.js
+++ js/action.js
@@ -29,6 +29,22 @@ function main() {
       { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
     ],
   });
+
+  let header = document.querySelector(".navbar");
+  let headerHeight = header.offsetHeight;
+
+  window.onscroll = function () {
+    let windowTop = window.scrollY;
+    // 스크롤 세로값이 헤더높이보다 크거나 같으면
+    // 헤더에 클래스 'drop'을 추가한다
+    if (windowTop >= headerHeight) {
+      header.classList.add("drop");
+    }
+    // 아니면 클래스 'drop'을 제거
+    else {
+      header.classList.remove("drop");
+    }
+  };
 }
 
 function sub() {
