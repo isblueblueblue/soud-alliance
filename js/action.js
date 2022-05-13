eb9151d28fd717488f9d3eeb033fb3f915f4ce81
isblueblueblue <isblueblueblue@gmail.com>
2022-05-13 14:37:37 +0900

    Update contact.css

--- js/action.js
+++ js/action.js
@@ -1,52 +1,62 @@
-$(function () {
-  common();
-  main();
-  sub();
+$(".event__items").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+  prevArrow:
+    "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+      },
+    },
+    { breakpoint: 600, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+    { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+  ],
 });
 
-function common() {}
+AOS.init({
+  duration: 800,
+  easing: "ease",
+  once: true,
+  offset: -100,
+});
 
-function main() {
-  $(".event__items").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: true,
-    prevArrow:
-      "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
-    nextArrow:
-      "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          dots: true,
-        },
-      },
-      { breakpoint: 600, settings: { slidesToShow: 2, slidesToScroll: 2 } },
-      { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
-    ],
+$(function () {
+  floatingLabel();
+});
+
+//navbar-motion
+let header = document.querySelector(".navbar");
+let headerHeight = header.offsetHeight;
+
+window.onscroll = function () {
+  let windowTop = window.scrollY;
+  // 스크롤 세로값이 헤더높이보다 크거나 같으면
+  // 헤더에 클래스 'drop'을 추가한다
+  if (windowTop >= headerHeight) {
+    header.classList.add("drop");
+  }
+  // 아니면 클래스 'drop'을 제거
+  else {
+    header.classList.remove("drop");
+  }
+};
+
+// form-motion
+var floatingLabel = function () {
+  $(".form-control").on("input", function () {
+    var $field = $(this).closest(".form-group");
+    if (this.value) {
+      $field.addClass("field--not-empty");
+    } else {
+      $field.removeClass("field--not-empty");
+    }
   });
-
-  let header = document.querySelector(".navbar");
-  let headerHeight = header.offsetHeight;
-
-  window.onscroll = function () {
-    let windowTop = window.scrollY;
-    // 스크롤 세로값이 헤더높이보다 크거나 같으면
-    // 헤더에 클래스 'drop'을 추가한다
-    if (windowTop >= headerHeight) {
-      header.classList.add("drop");
-    }
-    // 아니면 클래스 'drop'을 제거
-    else {
-      header.classList.remove("drop");
-    }
-  };
-}
-
-function sub() {
-  AOS.init();
-}
+};
---
+++ js/action00.js
@@ -0,0 +1,70 @@
+$(function () {
+  common();
+  main();
+  sub();
+});
+
+function common() {}
+
+function main() {
+  $(".event__items").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    prevArrow:
+      "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: true,
+        },
+      },
+      { breakpoint: 600, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+      { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+    ],
+  });
+
+  let header = document.querySelector(".navbar");
+  let headerHeight = header.offsetHeight;
+
+  window.onscroll = function () {
+    let windowTop = window.scrollY;
+    // 스크롤 세로값이 헤더높이보다 크거나 같으면
+    // 헤더에 클래스 'drop'을 추가한다
+    if (windowTop >= headerHeight) {
+      header.classList.add("drop");
+    }
+    // 아니면 클래스 'drop'을 제거
+    else {
+      header.classList.remove("drop");
+    }
+  };
+}
+
+function sub() {
+  AOS.init({
+    duration: 800,
+    easing: "ease",
+    once: true,
+    offset: -100,
+  });
+
+  floatingLabel();
+
+  var floatingLabel = function () {
+    $(".form-control").on("input", function () {
+      var $field = $(this).closest(".form-group");
+      if (this.value) {
+        $field.addClass("field--not-empty");
+      } else {
+        $field.removeClass("field--not-empty");
+      }
+    });
+  };
+}
