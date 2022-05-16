8a7351f19180b27ef2193b94e62dea803c708882
isblueblueblue <isblueblueblue@gmail.com>
2022-05-16 13:54:30 +0900

    Add index.html instagram part

--- js/action.js
+++ js/action.js
@@ -1,25 +1,25 @@
-$(".event__items").slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  arrows: true,
-  prevArrow:
-    "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
-  nextArrow:
-    "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: true,
-      },
-    },
-    { breakpoint: 600, settings: { slidesToShow: 2, slidesToScroll: 2 } },
-    { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
-  ],
-});
+// $(".event__items").slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
+//   arrows: true,
+//   prevArrow:
+//     "<button type='button' class='slick-prev'><i class='fa-solid fa-chevron-left'></i></button>",
+//   nextArrow:
+//     "<button type='button' class='slick-next'><i class='fa-solid fa-chevron-right'></i></button>",
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         dots: true,
+//       },
+//     },
+//     { breakpoint: 600, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+//     { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+//   ],
+// });
 
 AOS.init({
   duration: 800,
